be153ee1c49ff8983a3f8f6098da0b98cd295364
Svetlana184 <sveta.kgd.39@gmail.com>
2025-10-29 18:10:48 +0200

    login

---
+++ Session2/View/RegistWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistWindow.xaml
+    /// </summary>
+    public partial class RegistWindow : Window
+    {
+        public RegistWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Hyperlink_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+        }
+    }
+}
