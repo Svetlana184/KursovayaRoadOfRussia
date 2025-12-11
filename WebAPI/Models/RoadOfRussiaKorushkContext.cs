efab4ac94b89c96cb61161dc75ade9227cf2dcfd
Svetlana184 <sveta.kgd.39@gmail.com>
2025-12-11 23:46:51 +0200

    last

--- WebAPI/Models/RoadOfRussiaKorushkContext.cs
+++ WebAPI/Models/RoadOfRussiaKorushkContext.cs
@@ -39,7 +39,7 @@ public partial class RoadOfRussiaKorushkContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=GoblinsComp3;Initial Catalog=RoadOfRussiaKorushk;Persist Security Info=True;User ID=sa;Password=1234;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=TeacherPC;Initial Catalog=RoadOfRussiaKorushk;Persist Security Info=True;User ID=user12;Password=user12;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
